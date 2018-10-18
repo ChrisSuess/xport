9aeff22f748b6c6d482b764de06f3dd263ed54f6
Charlie Laughton <charles.laughton@nottingham.ac.uk>
2018-10-18 08:11:47 +0100

    Initial commit of xport

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    try:
+        root = __file__
+        if os.path.islink(root):
+            root = os.path.realpath(root)
+        ROOT = os.path.dirname(os.path.abspath(root))
+    except:
+        raise OSError('Error: caannot determine root directory')
+    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER') or os.path.join(ROOT, '/webjobs')
+    
